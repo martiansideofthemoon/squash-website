07c224b62283cdf136d48fd38c91bacee4e7d8df
Kalpesh Krishna <kalpeshk2011@gmail.com>
2019-07-12 12:14:16 -0400

    Finalize APIs

--- squash-backend/app.py
+++ squash-backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import request
 import json
 import datetime
+import os
 import secrets
 
 app = Flask(__name__)
@@ -26,8 +27,25 @@ def get_squash():
 @app.route('/request_squash', methods=['POST'])
 def request_squash():
     form_data = json.loads(request.data.decode('utf-8'))
-
     keygen = secrets.token_urlsafe(12)
+
+    form_data["timestamp"] = str(datetime.datetime.now())
+    form_data["key"] = keygen
+
+    # Filter out extra newline characters between paragraphs
+    input_text_list = [x for x in form_data["input_text"].split("\n") if len(x.strip()) > 0]
+    # Filter out input text with more than 4 paragraphs
+    input_text_list = input_text_list[:4]
+    # Finally, truncate paragraphs with more than 10000 characters
+    form_data["input_text"] = "\n".join([x[:10000] for x in input_text_list])
+
+    with open("../../squash-generation/squash/temp/queue.txt", "a") as f:
+        f.write("%s\n" % keygen)
+
+    os.mkdir("../../squash-generation/squash/temp/%s" % keygen)
+
+    with open("../../squash-generation/squash/temp/%s/metadata.json" % keygen, "w") as f:
+        f.write(json.dumps(form_data))
 
     response = flask.jsonify({
         "new_id": keygen
